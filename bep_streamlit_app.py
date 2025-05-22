bff40cb246d4753f0db32396e1a71bb5182765ae
Kensuke Yoshitomi <GoriGori0924@icloud.com>
2025-05-22 14:20:08 +0900

    Update bep_streamlit_app.py

--- bep_streamlit_app.py
+++ bep_streamlit_app.py
@@ -1,9 +1,11 @@
 
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import matplotlib.ticker as ticker
-import japanize_matplotlib
 import numpy as np
+
+mpl.rcParams['font.family'] = 'IPAexGothic' 
 
 st.set_page_config(page_title="損益分岐点シミュレーター", layout="centered")
 
