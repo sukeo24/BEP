60588788e5208c0faddc671bb4f16f92ec61092a
Kensuke Yoshitomi <GoriGori0924@icloud.com>
2025-05-29 17:23:52 +0900

    Update bep_streamlit_app.py

--- bep_streamlit_app.py
+++ bep_streamlit_app.py
@@ -126,7 +126,7 @@ else:
             xaxis=dict(tickformat=".1f"),
             plot_bgcolor="white",
             legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
-            height=500
+            height=600
         )
 
         st.plotly_chart(fig, use_container_width=True)
